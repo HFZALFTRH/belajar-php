babc7e6111ff02f0745440dd4ba99f357158ba24
HFZALFTRH <hafizalfitrah200@gmail.com>
2023-05-09 08:51:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport"content="width=device-width,initial-scale=1.0">
+
+<title> belajar php </title>
+</head>
+<body>
+    <?php
+    echo "belajar php";
+    echo "\n";
+    $name = "belajar php menggunakan var";
+    echo $name;
+    ?>
+    
+</body>
+ 
+<header>
